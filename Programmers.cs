ed9aa33a96b6603200e7ad843ab6caa36908381d
aleifj <sonmja96@naver.com>
2024-09-04 15:51:21 +0900

    0904 수정
    ㅇㅇ

--- Programmers.cs
+++ Programmers.cs
@@ -13,11 +13,10 @@ namespace aleifj
     {
         public int Solution09042(int[] numbers)//최댓값 만들기(2)
         {//정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소 중 두 개를 곱해 만들 수 있는 최댓값을 return하도록 solution 함수를 완성해주세요.
-            int answer = 0;
+            int answer = -100000000;//아니 이게 뭔데
         
             for(int i = 0; i < numbers.Length - 1; i++)
-            {
-                
+            {                
                 for(int j = i + 1; j < numbers.Length; j++)
                 {
                     if(numbers[i] < 0 && numbers[j] < 0)
